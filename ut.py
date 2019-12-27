c887418970b7dfb7a935d63fb418bf682373247d
Madan Nain <mnain@yahoo.com>
2019-12-27 16:26:22 -0500

    Add ut.py as simple Unit Test example

---
+++ ut.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import unittest
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+
+class MyUSPTOTest(unittest.TestCase):
+        def setup(self):
+                pass
+
+        def testLogin(self):
+                self.assertTrue(True)
+
+        def testLogOff(self):
+                self.assertTrue(True)
+
+        def tearDown(self):
+                pass
+
+        def testOnce(self):
+                self.assertTrue(True)
+
+if __name__ == "__main__":
+        unittest.main()
